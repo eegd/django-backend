0231f71db65cc6416f2a26ffecae235a554e7de4
alex <alex@alex-Mac-mini.local>
2024-08-15 18:37:43 +0800

    Multiple shopping lists

--- shopping_list/migrations/0001_initial.py
+++ shopping_list/migrations/0001_initial.py
@@ -1,4 +1,6 @@
 from django.db import migrations, models
+
+import django.db.models.deletion
 import uuid
 
 
@@ -10,6 +12,18 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name="ShoppingList",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4, primary_key=True, serialize=False
+                    ),
+                ),
+                ("name", models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
             name="ShoppingItem",
             fields=[
                 (
@@ -20,6 +34,14 @@ class Migration(migrations.Migration):
                 ),
                 ("name", models.CharField(max_length=100)),
                 ("purchased", models.BooleanField()),
+                (
+                    "shopping_list",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="shopping_items",
+                        to="shopping_list.shoppinglist",
+                    ),
+                ),
             ],
         ),
     ]
