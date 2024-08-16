a52157f084806b88a918809091421837d0797c4c
alex <alex@alex-Mac-mini.local>
2024-08-16 18:31:50 +0800

    Add test case

--- shopping_list/tests.py
+++ shopping_list/tests.py
@@ -1,1 +1,20 @@
-from django.test import TestCase
+import pytest
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from shopping_list.models import ShoppingList
+
+
+@pytest.mark.django_db
+def test_valid_shoppping_list_is_created():
+    url = reverse("all-shopping-lists")
+    data = {
+        "name": "Groceries",
+    }
+    client = APIClient()
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert ShoppingList.objects.get().name == "Groceries"
