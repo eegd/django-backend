f4f4e70e060fb4993bf523537b49e43b7610d68a
alex <alex@alex-Mac-mini.local>
2024-09-23 11:07:45 +0800

    Reformat shopping list

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -15,9 +15,7 @@ class ShoppingItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100)
     purchased = models.BooleanField()
-    shopping_list = models.ForeignKey(
-        ShoppingList, on_delete=models.CASCADE, related_name="shopping_items"
-    )
+    shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE, related_name="shopping_items")
 
     def __str__(self):
         return self.name
