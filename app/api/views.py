6257ecc2b0ff4ff0fb2174f3a75ec17363f2384a
alex.lin <alex@mac.local>
2024-10-13 16:42:17 +0800

    Add and Rewrite endpoint

--- app/api/views.py
+++ app/api/views.py
@@ -11,13 +11,19 @@ from app.models import ShoppingItem, ShoppingList
 
 
 class ListAddShoppingList(generics.ListCreateAPIView):
-    queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
 
     def perform_create(self, serializer):
         shopping_list = serializer.save()
         shopping_list.members.add(self.request.user)
+
         return shopping_list
+
+    def get_queryset(self):
+        members = self.request.user
+        queryset = ShoppingList.objects.filter(members=members)
+
+        return queryset
 
 
 class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -26,10 +32,15 @@ class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [ShoppingListMembersOnly]
 
 
-class AddShoppingItem(generics.CreateAPIView):
-    queryset = ShoppingItem.objects.all()
+class ListAddShoppingItem(generics.ListCreateAPIView):
     serializer_class = ShoppingItemSerializer
     permission_classes = [AllShoppingItemsShoppingListMembersOnly]
+
+    def get_queryset(self):
+        shopping_list = self.kwargs["pk"]
+        queryset = ShoppingItem.objects.filter(shopping_list=shopping_list)
+
+        return queryset
 
 
 class ShoppingItemDetail(generics.RetrieveUpdateDestroyAPIView):
